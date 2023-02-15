8d1b0ef836960f2b59067ee718732de4995c4872
Divyani Hazari <dhazari210@gmail.com>
2023-02-15 14:39:23 -0600

    added a another function with the all cases that runs the game.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,31 @@
 function getComputerChoice(){
-    var choices = ['rock', 'paper', 'scissors'];  
+    const choices = ['rock', 'paper', 'scissors'];  
     var randomIndex = Math.floor(Math.random() * 3);
-    console.log(choices[randomIndex]);
+    return choices[randomIndex];
 }
 
-getComputerChoice();
+function playRound(playerChoice, computerChoice){
+    playerChoice = playerChoice.toLowerCase();
+    if (playerChoice == computerChoice) {
+        return "Draw. Play again!";
+    } else {
+        if(playerChoice == "rock" && computerChoice == "paper"){
+            return "You Lose! Paper beats Rock";
+        } else if(playerChoice == "rock" && computerChoice == "scissors"){
+            return "You win! rock beats scissors";
+        } else if(playerChoice == "paper" && computerChoice == "scissors"){
+            return "You lose! Scissors beats Paper";
+        } else if(playerChoice == "paper" && computerChoice == "rock"){
+            return "You won! Paper beats Rock";    
+        } else if(playerChoice == "scissors" && computerChoice == "paper"){
+            return "You won! Scissors beats Paper";
+        } else if(playerChoice == "scissors" && computerChoice == "rock"){
+            return "You lose! rock beats scissors";
+        }
+    }
+}
+
+const computerChoice = getComputerChoice();
+const playerChoice = 'rock';
+const result = playRound(playerChoice, computerChoice);
+console.log(result);
